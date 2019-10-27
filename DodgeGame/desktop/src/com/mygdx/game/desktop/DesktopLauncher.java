90c9e60e81d29b8331bbbeaa58d1fb9e442bb12f
Amrit-S <aksingh@ucsd.edu>
2019-10-26 18:13:13 -0700

    added working gradle project

--- DodgeGame/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ DodgeGame/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new HelloWorld(), config);
+//		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
